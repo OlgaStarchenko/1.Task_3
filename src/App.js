801405c823fafe2b932bf236ea38dbd860b94988
OlgaStarchenko <o9915530@gmail.com>
2026-03-19 15:51:28 +1000

    I created the Button.jsx and ButtonRow.jsx components, working out the architecture of each component; I created style files for each component, Button.module.css and ButtonRow.module.css; I defined the main classes and included the files in the component via import. I also imported the Button.jsx and ButtonRow.jsx components into the main App.jsx component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-import "./App.css";
+import styles from "./App.module.css";
+import ButtonRow from "./components/ButtonRow";
 
 export function App() {
-  return null;
+  return (
+    <div className={styles.container}>
+      <div className={styles.calculator}>
+        <h1>OlgaStar</h1>
+        <div className={styles.calculatorDisplay}>
+          <input type="number" className={styles.displayInput}></input>
+        </div>
+        <div className={styles.calculatorButtonsPanel}>
+          <div className={styles.buttonFirstRow}>
+            <ButtonRow />
+            <ButtonRow />
+            <ButtonRow />
+            <ButtonRow />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/ButtonRow.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+
+export default function ButtonRow() {
+  return (
+    <div>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
