438cb9f41a159dcbe987158a7a1f515c128838a7
OlgaStarchenko <o9915530@gmail.com>
2026-03-19 17:21:33 +1000

    I added a NUMS array that stores button names from 0 to 9, displayed buttons with numeric values ​​on the screen using the map method, added action buttons +, -, = and a reset button C to the markup. I wrote styles in CSS for the container of number buttons .numberButtonContainer, for the container of action buttons .actionButtonContainer, and for the reset button C .resetButton

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import styles from "./App.module.css";
 
 export function App() {
+  const NUMS = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"];
+
   return (
     <div className={styles.container}>
       <div className={styles.calculator}>
         <h1>OlgaStar</h1>
+
         <div className={styles.calculatorDisplay}>
           <input type="number" className={styles.displayInput}></input>
         </div>
+
         <div className={styles.calculatorButtonsPanel}>
-          <div>
-            <button className={styles.button}>1</button>
+          <div className={styles.numberButtonContainer}>
+            {NUMS.map((num) =>
+              num === "0" ? (
+                <button className={styles.button}>{num}</button>
+              ) : (
+                <button className={styles.button}>{num}</button>
+              ),
+            )}
+          </div>
+
+          <div className={styles.actionButtonContainer}>
+            <button className={styles.resetButton}>C</button>
+            <button className={styles.button}>+</button>
+            <button className={styles.button}>-</button>
+            <button className={styles.button}>=</button>
           </div>
         </div>
       </div>
